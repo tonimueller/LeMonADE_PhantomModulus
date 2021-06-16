42d2fcb5c059aa060d911514a85cc161403f1620
Toni Mueller <mueller-toni@ipfdd.de>
2021-06-16 13:27:27 +0200

    add the settre and getter for the filename and the relaxation factor to the base class

--- include/LeMonADE_PM/updater/moves/MoveForceEquilibriumBase.h
+++ include/LeMonADE_PM/updater/moves/MoveForceEquilibriumBase.h
@@ -88,6 +88,16 @@ class MoveForceEquilibriumBase:public MoveBase
 	{
 		ShiftVector.setAllCoordinates(dx,dy,dz);
 	}
+
+    void setFilename(std::string filename_){static_cast<SpecializedMove*>(this)->setFilename(filename_);}
+    //! get the filename for the force extension data 
+    std::string const getFilename(){static_cast<SpecializedMove*>(this)->getFilename();}
+    
+    //! set the relaxation parameter for the cross link
+    void setRelaxationParameter(double relaxationChain_){static_cast<SpecializedMove*>(this)->setRelaxationParameter(relaxationChain_);}
+    //! get the relaxation parameter for the cross link 
+    double getRelaxationParameter(){static_cast<SpecializedMove*>(this)->getRelaxationParameter();} 
+
 	//! Random Number Generator (RNG)
 	RandomNumberGenerators randomNumbers;
 
