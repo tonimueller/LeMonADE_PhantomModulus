df87c7a726e2b6687ed0aa58d002aae3747ddc7f
Toni Mueller <mueller-toni@ipfdd.de>
2021-10-15 16:48:07 +0200

    start adding a start creator

---
+++ include/LeMonADE_PM/updater/UpdaterAddStars.h
@@ -0,0 +1,197 @@
+/*--------------------------------------------------------------------------------
+    ooo      L   attice-based  |
+  o\.|./o    e   xtensible     | LeMonADE: An Open Source Implementation of the
+ o\.\|/./o   Mon te-Carlo      |           Bond-Fluctuation-Model for Polymers
+oo---0---oo  A   lgorithm and  |
+ o/./|\.\o   D   evelopment    | Copyright (C) 2013-2015 by
+  o/.|.\o    E   nvironment    | LeMonADE Principal Developers
+    ooo                        |
+----------------------------------------------------------------------------------
+
+This file is part of LeMonADE.
+
+LeMonADE is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+LeMonADE is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with LeMonADE.  If not, see <http://www.gnu.org/licenses/>.
+
+--------------------------------------------------------------------------------*/
+
+#ifndef LEMONADE_UPDATER_SETUP_STARSAB
+#define LEMONADE_UPDATER_SETUP_STARSAB
+/**
+ * @file
+ *
+ * @class UpdaterAddStars
+ *
+ * @brief Updater to create a solution of monodisperse branched stars.
+ *
+ * @details This is a simple implementation of a system setup starting from an empty ingredients
+ * or a system with some monomers inside. This updater requires FeatureAttributes.
+ * Two tags are added to the monomers in alternating manner, usually needed for GPU computing.  // not realized in this first attempt
+ *                                                                                              // RS, 05 Nov 2019, but presumably 29 July 2020
+ *
+ * @tparam IngredientsType
+ *
+ * @param ingredients_ The system, holding either an empty simulation box for system setup
+ * or a prefilled ingredients where the stars shall be added
+ * @param NStar_ number of stars that are added to ingredients
+ * @param NMonoPerStar_ number of monomers in each star
+ * @param NMonoPerBranch_ number of monomers in each branch (excluding central monomer)
+ * @param NBranchPerStar_ number of branches in each star
+ * @param type1_ attribute tag of "even" monomers
+ * @param type2_ attribute tag of "odd" monomers
+ **/
+
+#include <LeMonADE/updater/UpdaterAbstractCreate.h>
+#include <LeMonADE/utility/Vector3D.h>
+#include <cmath>
+
+template<class IngredientsType>
+class UpdaterAddStars: public UpdaterAbstractCreate<IngredientsType>
+{
+  typedef UpdaterAbstractCreate<IngredientsType> BaseClass;
+
+public:
+  UpdaterAddStars(IngredientsType& ingredients_, uint32_t NStar_, 
+                  uint32_t NMonoPerBranch_, uint32_t NBranchPerStar_);
+
+  virtual void initialize();
+  virtual bool execute();
+  virtual void cleanup();
+
+  //! getter function for write compressed solvent bool
+  const bool getIsSolvent() const {return IsSolvent;}
+
+  //! getter function for number of stars
+  const int32_t getNStar() const {return NStar;}
+
+  //! getter function for number of monomers in branch
+  const int32_t getNMonoPerBranch() const {return NMonoPerBranch;}
+
+  //! getter function for number of branches in stars
+  const int32_t getNBranchPerStar() const {return NBranchPerStar;}
+
+  //! getter function for calculated density
+  const double getDensity() const {return density;}
+
+private:
+  // provide access to functions of UpdaterAbstractCreate used in this updater
+  using BaseClass::ingredients;
+  using BaseClass::addMonomerToParent;
+  using BaseClass::addSingleMonomer;
+  using BaseClass::linearizeSystem;
+
+  //! number of stars in the box
+  uint32_t NStar;
+
+  //! number of monomers in a branch
+  uint32_t NMonoPerBranch;  
+
+  //! number of branches in a star
+  uint32_t NBranchPerStar;
+
+  //! lattice occupation density
+  double density;
+
+  //! bool for execution
+  bool wasExecuted;
+};
+
+/**
+* @brief Constructor handling the new systems paramters
+* 
+* @param ingredients_ a reference to the IngredientsType - mainly the system
+* @param NStar_ number of stars to be added in the system instead of solvent
+* @param NBranchPerStar_ number of branches in each star
+* @param NMonoPerBranch_ number of monomers in each branch (excluding central monomer)
+*/
+
+
+template < class IngredientsType >
+UpdaterAddStars<IngredientsType>::UpdaterAddStars(
+    IngredientsType& ingredients_, 
+    uint32_t NStar_, 
+    uint32_t NMonoPerBranch_, 
+    uint32_t NBranchPerStar_
+    ):
+    BaseClass(ingredients_), NStar(NStar_), NMonoPerBranch(NMonoPerBranch_), NBranchPerStar(NBranchPerStar_), 
+    density(0.0), wasExecuted(false),
+    {}
+
+/**
+* @brief initialise function, calculate the target density to compare with at the end.
+*
+* @tparam IngredientsType Features used in the system. See Ingredients.
+*/
+template < class IngredientsType >
+void UpdaterAddStars<IngredientsType>::initialize(){
+  std::cout << "initialize UpdaterAddStars" << std::endl;
+
+  // get the target density from the sum of existing monomers and the new added chains
+  density=(double)( ingredients.getMolecules().size() + (NMonoPerBranch*NBranchPerStar+1) *NStar ) * 8  /(double)( ingredients.getBoxX()*ingredients.getBoxY()*ingredients.getBoxZ() );
+
+  std::cout << "add "<<NStar*(NMonoPerBranch*NBranchPerStar+1)<<" monomers to the box"<<std::endl;
+
+  execute();
+}
+
+/**
+* @brief Execution of the system creation
+*
+* @tparam IngredientsType Features used in the system. See Ingredients.
+*/
+template < class IngredientsType >
+bool UpdaterAddStars<IngredientsType>::execute(){
+    if(wasExecuted) return true;
+    std::cout << "execute UpdaterAddStars" << std::endl;
+    //loop over stars and star monomers and build it up
+    uint32_t gen;
+    uint32_t index_center;
+
+    for(uint32_t i=0;i<(NStar);i++){
+        addSingleMonomer(1);
+        for(uint32_t j=0;j<(NBranchPerStar);j++){
+            for(uint32_t b=0;b<(NMonoPerBranch);b++){
+                u_int32_t parentID;
+                if (b==0)
+                parentID = (ingredients.getMolecules().size()-NBranchPerStar)
+                addMonomerToParent(parentID,1);
+            }
+        }
+    }
+
+    ingredients.synchronize();
+    double lattice_volume(ingredients.getBoxX()*ingredients.getBoxY()*ingredients.getBoxZ());
+    if(std::abs(density - ( (double)(ingredients.getMolecules().size()*8) / lattice_volume )) > 0.0000000001 ){
+        std::cout << density << " " <<( (ingredients.getMolecules().size()*8) / lattice_volume)<<std::endl;
+        throw std::runtime_error("UpdaterAddStars: number of monomers in molecules does not match the calculated number of monomers!");
+    }else{
+        std::cout << "real lattice occupation density =" << (8*ingredients.getMolecules().size()) / lattice_volume<<std::endl;
+        wasExecuted=true;
+        // if we allow for solvent compression AND added single monomers (solvent): compress solvent
+            linearizeSystem();
+        return true;
+    }
+}
+
+/**
+* @brief Standard clean up.
+*
+* @tparam IngredientsType Features used in the system. See Ingredients.
+*/
+template < class IngredientsType >
+void UpdaterAddStars<IngredientsType>::cleanup(){
+
+}
+
+
+#endif /* LEMONADE_UPDATER_SETUP_STARSAB */
---
+++ projects/IdealReferenceForceEquilibrium.cpp
@@ -0,0 +1,188 @@
+/*--------------------------------------------------------------------------------
+    ooo      L   attice-based  |
+  o\.|./o    e   xtensible     | LeMonADE: An Open Source Implementation of the
+ o\.\|/./o   Mon te-Carlo      |           Bond-Fluctuation-Model for Polymers
+oo---0---oo  A   lgorithm and  |
+ o/./|\.\o   D   evelopment    | Copyright (C) 2013-2015 by
+  o/.|.\o    E   nvironment    | LeMonADE Principal Developers (see AUTHORS)
+    ooo                        |
+----------------------------------------------------------------------------------
+
+This file is part of LeMonADE.
+
+LeMonADE is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+LeMonADE is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with LeMonADE.  If not, see <http://www.gnu.org/licenses/>.
+
+--------------------------------------------------------------------------------*/
+
+/****************************************************************************** 
+ * based on LeMonADE: https://github.com/LeMonADE-project/LeMonADE/
+ * author: Toni Müller
+ * email: mueller-toni@ipfdd.de
+ * project: LeMonADE-Phantom Modulus
+ *****************************************************************************/
+#include <iostream>
+#include <vector>
+#include <bitset>
+
+#include <LeMonADE/core/Ingredients.h>
+#include <LeMonADE/updater/UpdaterReadBfmFile.h>
+#include <LeMonADE/analyzer/AnalyzerWriteBfmFile.h>
+#include <LeMonADE/feature/FeatureMoleculesIOUnsaveCheck.h>
+#include <LeMonADE/feature/FeatureReactiveBonds.h>
+#include <LeMonADE/feature/FeatureFixedMonomers.h>
+#include <LeMonADE/feature/FeatureBox.h>
+#include <LeMonADE/feature/FeatureLabel.h>
+#include <LeMonADE/utility/TaskManager.h>
+#include <LeMonADE/utility/RandomNumberGenerators.h>
+
+#include <extern/catchorg/clara/clara.hpp>
+
+#include <LeMonADE_PM/updater/UpdaterForceBalancedPosition.h>
+#include <LeMonADE_PM/updater/UpdaterReadCrosslinkConnectionsTendomer.h>
+#include <LeMonADE_PM/updater/moves/MoveForceEquilibrium.h>
+#include <LeMonADE_PM/updater/moves/MoveNonLinearForceEquilibrium.h>
+#include <LeMonADE_PM/feature/FeatureCrosslinkConnectionsLookUpTendomers.h>
+#include <LeMonADE_PM/analyzer/AnalyzerEquilbratedPosition.h>
+#include <LeMonADE_PM/updater/UpdaterAffineDeformation.h>
+#include <LeMonADE_PM/updater/UpdaterAddStars.h>
+
+
+int main(int argc, char* argv[]){
+	try{
+		///////////////////////////////////////////////////////////////////////////////
+		///parse options///
+		std::string inputBFM("init.bfm");
+		std::string outputDataPos("CrosslinkPosition.dat");
+		std::string outputDataDist("ChainExtensionDistribution.dat");
+		std::string feCurve("");
+		double relaxationParameter(10.);
+		double threshold(0.5);
+		double factor(0.995);
+		double stretching_factor(1.0);
+		bool gauss(false);
+		
+		bool showHelp = false;
+		auto parser
+			= clara::detail::Opt(            inputBFM, "inputBFM (=inconfig.bfm)"                        ) ["-i"]["--input"            ] ("(required)Input filename of the bfm file"                                    ).required()
+			| clara::detail::Opt(       outputDataPos, "outputDataPos (=CrosslinkPosition.dat)"          ) ["-o"]["--outputPos"        ] ("(optional) Output filename of the crosslink ID and the equilibrium Position.").optional()
+			| clara::detail::Opt(      outputDataDist, "outputDataDist (=ChainExtensionDistribution.dat)") ["-c"]["--outputDist"       ] ("(optional) Output filename of the chain extension distribution."             ).optional()
+			| clara::detail::Opt(           threshold, "threshold"                                       ) ["-t"]["--threshold"        ] ("(optional) Threshold of the average shift. Default 0.5 ."                    ).optional()
+			| clara::detail::Opt(   stretching_factor, "stretching_factor (=1)"                          ) ["-l"]["--stretching_factor"] ("(optional) Stretching factor for uniaxial deformation. Default 1.0 ."        ).optional()
+			| clara::detail::Opt(             feCurve, "feCurve (="")"                                   ) ["-f"]["--feCurve"          ] ("(optional) Force-Extension curve. Default \"\"."                             ).required()
+			| clara::detail::Opt( relaxationParameter, "relaxationParameter (=10)"                       ) ["-r"]["--relax"            ] ("(optional) Relaxation parameter. Default 10.0 ."                             ).optional()
+			| clara::detail::Opt(               gauss, "gauss"                                           ) ["-g"]["--gauss"            ] ("(optional) Deforma with a Gaussian deformation behaviour. Default 1.0 ."     ).optional()
+			| clara::Help( showHelp );
+		
+	    auto result = parser.parse( clara::Args( argc, argv ) );
+	    
+	    if( !result ) {
+	      std::cerr << "Error in command line: " << result.errorMessage() << std::endl;
+	      exit(1);
+	    }else if(showHelp == true){
+	      std::cout << "Simulator to connect linear chains with single monomers of certain functionality"<< std::endl;
+	      parser.writeToStream(std::cout);
+	      exit(0);
+	    }else{
+	      std::cout << "outputData            : " << outputDataPos          << std::endl;
+	      std::cout << "outputDataDist        : " << outputDataDist         << std::endl;
+	      std::cout << "inputBFM              : " << inputBFM               << std::endl; 
+	      std::cout << "threshold             : " << threshold              << std::endl; 
+		  std::cout << "feCurve               : " << feCurve                << std::endl;
+		  std::cout << "gauss                 : " << gauss                  << std::endl;
+		  std::cout << "stretching_factor     : " << stretching_factor      << std::endl;
+	    }
+		RandomNumberGenerators rng;
+		// rng.seedDefaultValuesAll();
+		rng.seedAll();
+		///////////////////////////////////////////////////////////////////////////////
+		///end options parsing
+		///////////////////////////////////////////////////////////////////////////////
+		//Read in th last Config 
+		typedef LOKI_TYPELIST_4(FeatureMoleculesIOUnsaveCheck, FeatureLabel, FeatureReactiveBonds,FeatureFixedMonomers) Features;
+		typedef ConfigureSystem<VectorInt3,Features, 7> Config;
+		typedef Ingredients<Config> Ing;
+		Ing myIngredients;
+		
+		TaskManager taskmanager;
+		
+		taskmanager.addUpdater( new UpdaterAddStars<Ing>(inputBFM,myIngredients, UpdaterReadBfmFile<Ing>::READ_LAST_CONFIG_SAVE),0);
+
+		//initialize and run
+		taskmanager.initialize();
+		taskmanager.run(1);
+		taskmanager.cleanup();
+		std::cout << "Read in conformation and go on to bring it into equilibrium forces..." <<std::endl;
+		//the foce equilibrium is reached off lattice ( no integer values for the positions )
+		typedef LOKI_TYPELIST_3(FeatureBox, FeatureCrosslinkConnectionsLookUpTendomers ,FeatureLabel) Features2;
+		typedef ConfigureSystem<VectorDouble3,Features2, 7> Config2;
+		typedef Ingredients<Config2> Ing2;
+		Ing2 myIngredients2;
+		
+		myIngredients2.setBoxX(myIngredients.getBoxX());
+		myIngredients2.setBoxY(myIngredients.getBoxY());
+		myIngredients2.setBoxZ(myIngredients.getBoxZ());
+		myIngredients2.setPeriodicX(myIngredients.isPeriodicX());
+		myIngredients2.setPeriodicY(myIngredients.isPeriodicY());
+		myIngredients2.setPeriodicZ(myIngredients.isPeriodicZ());
+		myIngredients2.modifyMolecules().resize(myIngredients.getMolecules().size());
+		myIngredients2.modifyMolecules().setAge(myIngredients.getMolecules().getAge());
+        
+        myIngredients2.setNumTendomers           (myIngredients.getNumTendomers());
+		myIngredients2.setNumCrossLinkers        (myIngredients.getNumCrossLinkers());
+		myIngredients2.setNumMonomersPerChain    (myIngredients.getNumMonomersPerChain());
+		myIngredients2.setNumLabelsPerTendomerArm(myIngredients.getNumLabelsPerTendomerArm());
+		
+		for(size_t i = 0; i< myIngredients.getMolecules().size();i++){
+			myIngredients2.modifyMolecules()[i].modifyVector3D()=myIngredients.getMolecules()[i].getVector3D();
+			myIngredients2.modifyMolecules()[i].setReactive(myIngredients.getMolecules()[i].isReactive());
+			myIngredients2.modifyMolecules()[i].setNumMaxLinks(myIngredients.getMolecules()[i].getNumMaxLinks());
+			for (size_t j = 0 ; j < myIngredients.getMolecules().getNumLinks(i);j++){
+				uint32_t neighbor(myIngredients.getMolecules().getNeighborIdx(i,j));
+				if( ! myIngredients2.getMolecules().areConnected(i,neighbor) )
+					myIngredients2.modifyMolecules().connect(i,neighbor);
+			}
+		}
+		myIngredients2.synchronize();
+
+		TaskManager taskmanager2;
+		taskmanager2.addUpdater( new UpdaterAffineDeformation<Ing2>(myIngredients2, stretching_factor),0 );
+		//read bonds and positions stepwise
+        auto updater = new UpdaterForceBalancedPosition<Ing2,MoveNonLinearForceEquilibrium>(myIngredients2, threshold) ;
+        updater->setFilename(feCurve);
+        updater->setRelaxationParameter(relaxationParameter);
+        auto updater2 = new UpdaterForceBalancedPosition<Ing2,MoveForceEquilibrium>(myIngredients2, threshold) ;
+		if ( gauss == 0 ){
+			std::cout << "TendomerNetworkForceEquilibrium: add UpdaterForceBalancedPosition<Ing2,MoveNonLinearForceEquilibrium>(myIngredients2, threshold) \n";
+        	taskmanager2.addUpdater( updater );
+		}else {
+			std::cout << "TendomerNetworkForceEquilibrium: add UpdaterForceBalancedPosition<Ing2,MoveForceEquilibrium>(myIngredients2, threshold) \n";
+			taskmanager2.addUpdater( updater2 );
+		}
+		taskmanager2.addAnalyzer(new AnalyzerEquilbratedPosition<Ing2>(myIngredients2,outputDataPos, outputDataDist));
+		//initialize and run
+		taskmanager2.initialize();
+		taskmanager2.run();
+		taskmanager2.cleanup();
+		
+	}
+	catch(std::exception& e){
+		std::cerr<<"Error:\n"
+		<<e.what()<<std::endl;
+	}
+	catch(...){
+		std::cerr<<"Error: unknown exception\n";
+	}
+	
+	return 0;
+}
