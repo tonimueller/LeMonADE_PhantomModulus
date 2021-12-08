dcd5bc1d06c9af11ce20e6722a4759608e2624cb
Toni Mueller <mueller-toni@ipfdd.de>
2021-12-08 09:03:01 +0100

    Preequilibrate with Gaussian force extension and afterwards turn on the non-linear

--- projects/ForceEquilibrium.cpp
+++ projects/ForceEquilibrium.cpp
@@ -174,6 +174,7 @@ int main(int argc, char* argv[]){
             taskmanager2.addUpdater( forceUpdater );
         }else{
             std::cout << "Use gaussian force-extension relation\n";
+            taskmanager2.addUpdater( new UpdaterForceBalancedPosition<Ing2,MoveForceEquilibrium>(myIngredients2, 0.001); );
             taskmanager2.addUpdater( forceUpdater2 );
         }
         taskmanager2.addAnalyzer(new AnalyzerEquilbratedPosition<Ing2>(myIngredients2,outputDataPos, outputDataDist));
